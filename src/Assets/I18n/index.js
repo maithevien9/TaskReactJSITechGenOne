3b4ccf9dfbf5b46d83d6f6c1e248d2763124b4bd
vienx <maithevien9@gmail.com>
2021-01-21 11:50:49 +0700

    done

--- src/Assets/I18n/index.js
+++ src/Assets/I18n/index.js
@@ -1,24 +1,18 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import en from './en';
-import vn from './vn';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./en";
+import vn from "./vn";
 
 const resources = {
-	en,
-	vn
+  en,
+  vn,
 };
 
 i18n
-	.use(initReactI18next) // passes i18n down to react-i18next
-	.init({
-		resources,
-		lng: 'vn',
-
-		keySeparator: false, // we do not use keys in form messages.welcome
-
-		interpolation: {
-			escapeValue: false // react already safes from xss
-		}
-	});
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: "vn",
+  });
 
 export default i18n;
